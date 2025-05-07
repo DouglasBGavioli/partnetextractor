239cee298f6e53d7b341b757ea11b039802a934f
DouglasGavioli <douglasbgavioli@hotmail.com>
2025-05-07 16:07:59 -0300

    fix: variaveis de ambiente

--- src/config/firebaseClient.ts
+++ src/config/firebaseClient.ts
@@ -3,12 +3,12 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: process.env.API_SECRET_KEY_FIREBASE_API_KEY,
-    authDomain: process.env.API_SECRET_KEY_FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.API_SECRET_KEY_FIREBASE_PROJECT_ID,
-    storageBucket: process.env.API_SECRET_KEY_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.API_SECRET_KEY_FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.API_SECRET_KEY_FIREBASE_APP_ID,
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 
